6999ff3f765046c62d63565ef95307dc3f0dd75d
jay <jay@hatterascomputers.com>
2015-09-10 22:57:47 -0400

    blah

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JS-Html-LottoSim
+
+
+So my mom buys these tickets every week. They have 5 numbers on them with an extra bonus number. I tell he it's a tool of the man and she should quit. As life is I'm trying to be a super noob-tube at HTML/CSS/JS and decided a simple simulator could maybe show her how many millions she would need to spend in order to re-coup. Alas the lottery isn't like lightening, it is unlikely to strike twice. Ever. Never.
---
+++ lottery.js
@@ -0,0 +1,55 @@
+var userNum = [];
+var userPowerBallNum = 0;
+var lottoNum = [];
+var lottoPowerBallNumber = 0;
+var years = 0;
+var tempTxt = "num";
+
+var week = [];
+var winning = [];
+
+var moneySpent = 0;
+
+//gathers random lottery numbers + powerball
+for (var i = 0; i < 5;i++){
+	lottoNum[i] = Math.floor((Math.random() * 75) + 1);
+}
+lottoNum.sort();	
+lottoPowerBallNumber = Math.floor((Math.random() * 15) + 1);
+//alert(lottoNum);
+
+function doWork(){
+	alert("Doing work son");
+	for(var i = 0; i < years; i++){
+		for (var j = 0; j < 53; j++){
+			//52 weeks per year shit goes here. 
+
+		}
+	}
+
+}
+
+//When user clicks quick pick
+function getRanNums(){
+	for (var i = 0; i < 5; i++){
+		userNum[i] = Math.floor((Math.random() * 75) + 1);
+	}
+	userNum.sort();
+	userPowerBallNum = Math.floor((Math.random() * 15) + 1);
+	alert("Your numbers : " + userNum + "\n PowerBall: " + userPowerBallNum);
+	doWork();
+}
+
+//Gets text boxes with numbers
+function getNums(){
+	var x = document.getElementById("nums");
+	for (var i = 0; i < 6; i++){
+		userNum[i] = x.elements[i].value;
+		//alert(userNum[i]);
+	}
+	userNum.sort();
+	userPowerBallNum = x.elements[5].value;
+	years = x.elements[6].value;
+	doWork();
+}
+
